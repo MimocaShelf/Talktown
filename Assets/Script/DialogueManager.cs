90afdc66a4cf795e8a2e54e1661c555a5ed90f2f
GamexRaider <gameraidershreyash@gmail.com>
2025-09-16 14:06:57 +1000

    NPC interact

---
+++ Assets/Script/DialogueManager.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager Instance;
+    public TextMeshProUGUI dialogueText;
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+    }
+
+    public void ShowDialogue(string message)
+    {
+        dialogueText.text = message;
+    }
+
+    public void ClearDialogue()
+    {
+        dialogueText.text = "";
+    }
+}
