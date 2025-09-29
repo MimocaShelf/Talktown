2726c533975612140fb513ceaa9b4524b3332eac
GamexRaider <gameraidershreyash@gmail.com>
2025-09-29 22:23:24 +1000

    Grocery NPC

--- Assets/Script/NPCInteract.cs
+++ Assets/Script/NPCInteract.cs
@@ -10,14 +10,28 @@ public class NPCInteract : MonoBehaviour
     {
         if (playerInRange && Input.GetKeyDown(KeyCode.E))
         {
-            DialogueManager.Instance.ShowDialogue(npcName + ": Welcome to Talktown!");
+            if (CompareTag("GroceryNPC"))
+            {
+                DialogueManager.Instance.ShowDialogue(npcName + ": What can I help you with today?");
 
-            DialogueChoiceManager.Instance.ShowChoices(
-                "Ask about the town", () => DialogueManager.Instance.ShowDialogue("This town is full of life!"),
-                "Ask about work", () => DialogueManager.Instance.ShowDialogue("There are many jobs in the market."),
-                "Ask about food", () => DialogueManager.Instance.ShowDialogue("Try the local café!"),
-                "Say goodbye", () => DialogueManager.Instance.ShowDialogue("Goodbye! Come back anytime.")
-            );
+                DialogueChoiceManager.Instance.ShowChoices(
+                    "I want rice", () => DialogueManager.Instance.ShowDialogue("NPC: Sure, the rice is in aisle 2."),
+                    "I want chips", () => DialogueManager.Instance.ShowDialogue("NPC: Sure, Chips are in the aisle 9."),
+                    "I want water", () => DialogueManager.Instance.ShowDialogue("NPC: Sure, Water bottles are near the drinks section, aisle 8."),
+                    "I want milk", () => DialogueManager.Instance.ShowDialogue("NPC: Sure, Milk is in the refrigerated section, aisle 6.")
+                );
+            }
+            else
+            {
+                DialogueManager.Instance.ShowDialogue(npcName + ": Welcome to Talktown!");
+
+                DialogueChoiceManager.Instance.ShowChoices(
+                    "Ask about the town", () => DialogueManager.Instance.ShowDialogue("This town is full of life!"),
+                    "Ask about groceries", () => DialogueManager.Instance.ShowDialogue("There is a grocery store behind me. Check it out!"),
+                    "Ask about food", () => DialogueManager.Instance.ShowDialogue("Try the local café!"),
+                    "Say goodbye", () => DialogueManager.Instance.ShowDialogue("Goodbye! Come back anytime.")
+                );
+            }
         }
     }
 
@@ -36,6 +50,7 @@ public class NPCInteract : MonoBehaviour
         {
             playerInRange = false;
             DialogueManager.Instance.ClearDialogue();
+            DialogueChoiceManager.Instance.HideChoices();
         }
     }
 }
--- Assets/WalldoffStudios/Shared/Scripts/ColorSetup1.cs
+++ Assets/WalldoffStudios/Shared/Scripts/ColorSetup1.cs
@@ -6,14 +6,10 @@ using UnityEditor;
 
 namespace WalldoffStudios.ToonCharacter
 {
-    public enum ColorUpdateType
-    {
-        OnClick = 0,
-        Always = 1,
-    }
-    
+
+
     [ExecuteInEditMode, CanEditMultipleObjects]
-    public class ColorSetup : MonoBehaviour
+    public class ColorSetup1 : MonoBehaviour
     {
         [SerializeField] private ToonColors toonColors = null;
         [SerializeField] private bool destroyAfterApplying = true;
@@ -23,7 +19,7 @@ namespace WalldoffStudios.ToonCharacter
         public MeshRenderer MeshRenderer { get; private set; }
         public SkinnedMeshRenderer SkinnedMeshRenderer { get; private set; }
         private MaterialPropertyBlock _materialPropertyBlock;
-        
+
         private readonly int SelfShadingSize = Shader.PropertyToID("_SelfShadingSize");
         private readonly int HighLightSmoothness = Shader.PropertyToID("_HighlightSmoothness");
         private readonly int HighlightStrength = Shader.PropertyToID("_HighlightStrength");
@@ -34,7 +30,7 @@ namespace WalldoffStudios.ToonCharacter
             toonColors = newToonColors;
             ApplyColors();
         }
-        
+
         public Color[] GetColors()
         {
             if (toonColors == null) return null;
@@ -59,7 +55,7 @@ namespace WalldoffStudios.ToonCharacter
 
         private void OnEnable()
         {
-            if(applyOnEnable == false) return;
+            if (applyOnEnable == false) return;
             if (toonColors == null)
             {
                 Debug.LogWarning("Toon colors isn't assigned");
@@ -73,39 +69,39 @@ namespace WalldoffStudios.ToonCharacter
         public void ApplyColors()
         {
             _materialPropertyBlock ??= new MaterialPropertyBlock();
-         
+
             Renderer renderer = GetActiveRenderer();
             if (renderer == null) throw new Exception($"The {gameObject.name} doesn't have a MeshRenderer or SkinnedMeshRenderer component");
             renderer.GetPropertyBlock(_materialPropertyBlock);
-         
+
             if (toonColors != null)
             {
                 for (int i = 0; i < toonColors.ColorCount; i++)
                 {
-                    _materialPropertyBlock.SetVector($"_Color{i+1}", toonColors.GetColorAtIndex(i));
+                    _materialPropertyBlock.SetVector($"_Color{i + 1}", toonColors.GetColorAtIndex(i));
                 }
                 _materialPropertyBlock.SetFloat(SelfShadingSize, toonColors.selfShadingSize);
                 _materialPropertyBlock.SetFloat(HighLightSmoothness, toonColors.highlightSmoothness);
                 _materialPropertyBlock.SetFloat(HighlightStrength, toonColors.highlightStrength);
                 _materialPropertyBlock.SetFloat(ShadowStrength, toonColors.shadowStrength);
             }
-         
-            
+
+
             renderer.SetPropertyBlock(_materialPropertyBlock);
-            
+
             if (destroyAfterApplying && Application.isPlaying) Destroy(this, 0.1f);
         }
-        
+
         private Renderer GetActiveRenderer()
         {
             Renderer renderer = null;
             if (MeshRenderer == null) MeshRenderer = GetComponent<MeshRenderer>();
             if (MeshRenderer != null) return MeshRenderer;
-         
+
             if (SkinnedMeshRenderer == null) SkinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
             if (SkinnedMeshRenderer != null) return SkinnedMeshRenderer;
-            
+
             return renderer;
         }
-    }   
+    }
 }
