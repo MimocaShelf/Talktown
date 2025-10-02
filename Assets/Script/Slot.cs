453f53929a342953c76aa0e15b902dd7da2e6a02
MimocaShelf <maiaangnixon@gmail.com>
2025-10-02 20:34:17 +1000

    Revert "Visual imporvemet UI"
    This reverts commit ed2108f32aa080cc5b4f88da4ff470fc429d4e42.

--- Assets/Script/Slot.cs
+++ Assets/Script/Slot.cs
@@ -1,18 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 using TMPro;
-using UnityEngine.UI;
 
 public class Slot : MonoBehaviour, IDropHandler
 {
     public GameObject currentWord;
-    private TextMeshProUGUI slotText;   
-    private Image background;
+    private TextMeshProUGUI slotText;
 
     private void Awake()
     {
         // Find the text component in this slot
-        background = GetComponent<Image>();
         slotText = GetComponentInChildren<TextMeshProUGUI>();
     }
 
@@ -25,33 +22,9 @@ public class Slot : MonoBehaviour, IDropHandler
             // Get the text from the dragged word
             TextMeshProUGUI wordText = eventData.pointerDrag.GetComponentInChildren<TextMeshProUGUI>();
 
-            DraggableWord draggable = eventData.pointerDrag.GetComponent<DraggableWord>();
-            if (draggable != null)
-            {
-                // Update the slot’s text
-                slotText.text = draggable.GetWord();
-
-                // Change slot background colour to blue
-                if (background != null)
-                    background.color = Color.blue;
-
-                Debug.Log($"Slot filled with: {slotText.text}");
-            }
-
-
             if (wordText != null && slotText != null)
             {
                 slotText.text = wordText.text;  // ✅ Display the word on the slot
-            }
-            Image wordImage = eventData.pointerDrag.GetComponent<Image>();
-            if (wordImage != null)
-            {
-                wordImage.color = Color.blue;
-            }
-            Image slotImage = GetComponent<Image>();
-            if (slotImage != null)
-            {
-                slotImage.color = Color.blue;
             }
 
             // Track which word is here
